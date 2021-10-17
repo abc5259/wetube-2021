3543b22f316316d9ed941eb89cce947d04d9a9fb
LeeJaeHoon <dlwogns3413@naver.com>
2021-10-17 14:07:12 +0900

    use morgan

--- src/server.js
+++ src/server.js
@@ -1,22 +1,17 @@
 import express from "express";
+import logger from "morgan";
 
 const PORT = 4000;
 
 const app = express();
 
 const handleHome = (req, res) => {
-  return res.send("<h1>i still love you</h1>");
-}
-const handleLogin = (req, res) => {
-  return res.send("Login here");
+  res.send("hello");
 }
 
-app.get("/", handleHome);
-app.get('/login', handleLogin);
+app.use(logger("dev"));
+app.get('/', handleHome);
 
 const handleListening = () => console.log(`✅ Server listening on port http://localhost:${PORT} 🔥`);
 
 app.listen(PORT, handleListening);
-
-
-//request -> app.get ->  
