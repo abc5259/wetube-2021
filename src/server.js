fededa418b00e71ca0a604ee798f8b233a758204
LeeJaeHoon <dlwogns3413@naver.com>
2021-10-17 14:49:14 +0900

    create Router global,users,videos

--- src/server.js
+++ src/server.js
@@ -1,16 +1,28 @@
 import express from "express";
-import logger from "morgan";
+import morgan from "morgan";
 
 const PORT = 4000;
 
 const app = express();
+const logger = morgan('dev');
+app.use(logger);
 
-const handleHome = (req, res) => {
-  res.send("hello");
-}
+const globalRouter = express.Router();
+const handleHome = (req, res) => res.send("Home");
+globalRouter.get("/", handleHome);
 
-app.use(logger("dev"));
-app.get('/', handleHome);
+const userRouter = express.Router();
+const handleEditUSer = (req, res) => res.send("Edit User");
+userRouter.get("/edit", handleEditUSer);
+
+const videoRouter = express.Router();
+const handleWatchVideo = (req, res) => res.send("Watch Video");
+videoRouter.get("/watch", handleWatchVideo);
+
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
+
 
 const handleListening = () => console.log(`✅ Server listening on port http://localhost:${PORT} 🔥`);
 
