751eacc4918f4dae3842d32179ba7dfffc67a29b
LeeJaeHoon <dlwogns3413@naver.com>
2021-10-17 11:11:17 +0900

    clear wetube set up and create server

---
+++ src/server.js
@@ -0,0 +1,22 @@
+import express from "express";
+
+const PORT = 4000;
+
+const app = express();
+
+const handleHome = (req, res) => {
+  return res.send("<h1>i still love you</h1>");
+}
+const handleLogin = (req, res) => {
+  return res.send("Login here");
+}
+
+app.get("/", handleHome);
+app.get('/login', handleLogin);
+
+const handleListening = () => console.log(`✅ Server listening on port http://localhost:${PORT} 🔥`);
+
+app.listen(PORT, handleListening);
+
+
+//request -> app.get ->  
