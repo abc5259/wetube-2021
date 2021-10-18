012d67b7e7a4f3e4a782396817915d488d6b285a
LeeJaeHoon <dlwogns3413@naver.com>
2021-10-18 15:11:18 +0900

    use pug mixin and each

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,15 +1,42 @@
+
 export const trending = (req, res) => {
-  res.render("home");
+  const videos = [
+    {
+      title:"First Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1,
+    },
+    {
+      title:"Second Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1,
+    },
+    {
+      title:"Third Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1,
+    }
+  ]
+  res.render("home", { pageTitle: "Home", videos});
 }
 
 export const see = (req, res) => {
   console.log(req.params);
-  res.render("watch");
+  res.render("watch", { pageTitle: "Video" });
 }
 
 export const edit = (req, res) => {
   console.log(req.params);
-  res.render("edit");
+  res.render("edit", { pageTitle: "Edit" });
 }
 
 export const search = (req, res) => {
