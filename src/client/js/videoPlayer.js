7caafb8532824eb769a2b5de0c80ed20c4203ebe
LeeJaeHoon <dlwogns3413@naver.com>
2021-10-27 01:54:12 +0900

    clear videoPlayer mute/play and working video time

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,8 +1,12 @@
 const video = document.querySelector("video");
 const playBtn = document.getElementById("play");
 const muteBtn = document.getElementById("mute");
-const time = document.getElementById("time");
-const volume = document.getElementById("volume");
+const volumeRange = document.getElementById("volume");
+const currentTime = document.getElementById("currentTime");
+const totalTime = document.getElementById("totalTime");
+
+let volumeValue = 0.5;
+video.volume = volumeValue;
 
 const handlePlayClick = e => {
   if (video.paused) {
@@ -10,19 +14,48 @@ const handlePlayClick = e => {
   } else {
     video.pause();
   }
+  playBtn.innerText = video.paused ? "Play" : "Pause";
 };
 
-const handlePause = () => {
-  playBtn.innerText = "play";
+const handleMute = e => {
+  if (video.muted) {
+    video.muted = false;
+  } else {
+    video.muted = true;
+  }
+  muteBtn.innerText = video.muted ? "Unmute" : "Mute";
+  volumeRange.value = video.muted ? 0 : volumeValue;
 };
 
-const handlePlay = () => {
-  playBtn.innerText = "pause";
+const handleVolumnChange = e => {
+  const {
+    target: { value },
+  } = e;
+  if (parseFloat(value) === 0) {
+    video.muted = true;
+  } else {
+    video.muted = false;
+  }
+  muteBtn.innerText = video.muted ? "Unmute" : "Mute";
+  volumeValue = value;
+  video.volume = volumeValue;
 };
 
-const handleMute = e => {};
+const handleLoadedMetadata = () => {
+  totalTime.innerText = Math.floor(video.duration);
+};
+
+const handleTimeUpdate = () => {
+  console.log(video.currentTime);
+  currentTime.innerText = Math.floor(video.currentTime);
+};
+
+if (video.readyState == 4) {
+  handleLoadedMetadata();
+}
 
 playBtn.addEventListener("click", handlePlayClick);
 muteBtn.addEventListener("click", handleMute);
-video.addEventListener("pause", handlePause);
-video.addEventListener("play", handlePlay);
+volumeRange.addEventListener("input", handleVolumnChange);
+video.addEventListener("loadedmetadata", handleLoadedMetadata);
+video.addEventListener("timeupdate", handleTimeUpdate);
