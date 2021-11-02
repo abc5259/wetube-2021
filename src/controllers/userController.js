805932386ef77888b00d5501041bd579759c9e3d
LeeJaeHoon <dlwogns3413@naver.com>
2021-11-03 02:36:35 +0900

    clear User Profile scss/js

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -227,13 +227,15 @@ export const postChangePassword = async (req, res) => {
 
 export const see = async (req, res) => {
   const { id } = req.params;
-  const user = await User.findById(id).populate({
-    path: "videos",
-    populate: {
-      path: "owner",
-      model: "User",
-    },
-  });
+  const user = await User.findById(id)
+    .populate({
+      path: "videos",
+      populate: {
+        path: "owner",
+        model: "User",
+      },
+    })
+    .populate("subscribe");
   if (!user) {
     req.flash("error", "User not found.");
     return res.status(404).render("404", { pageTitle: "User not found." });
